7e3b15b6a3e393f91b72e478175b2b28d308bccb
JavierDeSant <j.desantiagosoto@gmail.com>
2024-08-21 03:00:31 +0200

    Add Frontend Mentor challenge files

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,41 @@ function App() {
   return (
     <>
       <h1 className=" text-6xl">Contact Form</h1>
+
+      <body>
+        Contact Us
+
+        First Name
+        This field is required
+
+        Last Name
+        This field is required
+
+        Email Address
+        Please enter a valid email address
+        This field is required
+
+        Query Type
+        General Enquiry
+        Support Request
+        Please select a query type
+
+        Message
+        This field is required
+
+        I consent to being contacted by the team
+        To submit this form, please consent to being contacted
+
+        Submit
+
+        Message Sent!
+        Thanks for completing the form. We'll be in touch soon!
+        
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="hover:underline">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/javierdesant" target="_blank" className="hover:underline">JavierDeSant</a>.
+        </div>
+      </body>
     </>
   )
 }
